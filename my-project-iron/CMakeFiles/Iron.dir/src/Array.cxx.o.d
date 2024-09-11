79af8ff05a6053ebd54a8492dd1273b672fc6971
yves adonias <yves.adonias@epitech.eu>
2024-09-11 22:19:54 +0200

    nettoyage du dossier des makefiles inutiles

--- my-project-iron/CMakeFiles/Iron.dir/src/Array.cxx.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Iron.dir/src/Array.cxx.o: \
-  /storage/emulated/0/Documents/Cxxdroid/my-project-iron/src/Array.cxx \
-  /storage/emulated/0/Documents/Cxxdroid/my-project-iron/include/AffectsArray.hpp
--- my-project-iron/CMakeFiles/Iron.dir/src/IronIO.cxx.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Iron.dir/src/IronIO.cxx.o: \
-  /storage/emulated/0/Documents/Cxxdroid/my-project-iron/src/IronIO.cxx \
-  /storage/emulated/0/Documents/Cxxdroid/my-project-iron/include/IronIO.hpp
